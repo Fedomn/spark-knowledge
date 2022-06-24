eaf0e06af741e861b6276e46731d62d1dfd19c2d
Fedomn <fedomn.ma@gmail.com>
2022-06-24 22:52:55 +0800

    enable hive tables

--- pyspark/structure.py
+++ pyspark/structure.py
@@ -5,8 +5,14 @@ if __name__ == '__main__':
     spark = SparkSession.builder \
         .master("local[*]") \
         .appName("StructuredAPI") \
+        .config("spark.sql.warehouse.dir", './warehouse') \
+        .enableHiveSupport() \
         .getOrCreate()
 
     df = spark.read.format("json").load("./data/flight-data/json/2015-summary.json")
     df.printSchema()
     df.select("*").where("count > 1").orderBy(desc("count")).show(3)
+
+    new_df: DataFrame = df.select("*").where("count > 1")
+    new_df.write.mode('overwrite').format('orc').saveAsTable("filtered_data")
+    spark.sql("select * from filtered_data").printSchema()
